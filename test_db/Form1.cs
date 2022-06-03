f3364b2b36799a6d42a4249bf015346391e502ea
David <david.chalyan25@gmail.com>
2022-06-03 20:26:34 +0300

    Added a check for an existing user in the database

--- test_db/Form1.cs
+++ test_db/Form1.cs
@@ -42,24 +42,48 @@ namespace test_db
             SqlDataAdapter adapter = new SqlDataAdapter();
             DataTable table = new DataTable();
 
-            string querystring = $"select id_user, login_user, password_user from register where login_user = '{loginUser}' and password_user = '{passUser}'";
+            string querystring = $"insert into register(login_user, password_user) values ('{loginUser}', '{passUser}')";
             SqlCommand command = new SqlCommand(querystring, db.GetConnection());
+            db.OpenConnection();
+            checkUser();
+            
+        }
+        private Boolean checkUser()
+        {
+            var loginUser = textBox1.Text;
+            var pasUser = textBox2.Text;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+            string querystring = $"select id_user, login_user, password_user from register where login_user = '{loginUser}' and password_user = '{pasUser}'";
+            SqlCommand command = new SqlCommand(querystring, db.GetConnection());
+
             adapter.SelectCommand = command;
             adapter.Fill(table);
-            if (table.Rows.Count == 1)
+
+            if (table.Rows.Count > 0)
             {
-                MessageBox.Show("Вы успешно вошли!", "Успешно!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                Form1 frm1 = new Form1();
-                this.Hide();
-                frm1.ShowDialog();
-                this.Show();
+                MessageBox.Show("Пользователь уже зарегистрирован!");
+                return true;
             }
             else
             {
-                MessageBox.Show("Такого аккаунта не существует", "Аккаунта не существует!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    MessageBox.Show("Аккаунт успешно создан!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Form3 frm3 = new Form3();
+                    this.Hide();
+                    frm3.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Аккаунт не создан!", "Аккаунт не создан!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+                db.CloseConnection();
+                return false;
             }
         }
-
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             Form2 newfrm = new Form2();
@@ -75,5 +99,7 @@ namespace test_db
             textBox1.Text = "";
             textBox2.Text = "";
         }
+
+        
     }
 }
